b3e08b58d6c8c9737ead33272d8a8e573c5de211
Peter Haagsma <pjh26@gold12.cs.calvin.edu>
2019-03-13 16:57:08 -0400

    Added recursive functionality and conditionals to determine file type

--- Project02/CleanScript/clean.sh
+++ Project02/CleanScript/clean.sh
@@ -1,24 +1,97 @@
 #!/bin/sh
 
+cleanup() (
+	myDir="$PWD"
+	s=""
+	q=""
+	i=""
+	for input in $*
+	do
+		case $input in
+			-s | --stats 		)
+				s=" -s"
+				;;
 
-for input in $*
-do
-	case $input in
-		-s | --stats 		)
-			echo "Stats"
-			;;
+			-q | --quiet		)
+				q=" -q"
+				;;
 
-		-q | --quiet		)
-			echo "Quiet"
-			;;
+			-i | --interactive	)
+				i=" -i"
+				;;
 
-		-i | --interactive	)
-			echo "Interactive"
-			;;
+			*					)
+				if test -d $input;
+				then
+					myDir="$input"
+				else
+					echo "! ! ! Please enter a valid input ! ! !"
+					exit 1
+				fi
+				;;
+		esac
+	done
 
-		*					)
-			echo $input
-			;;
-	esac
+	cd $myDir
 
-done
+	for item in $(ls -a)
+	do
+
+		# This case statement ensures we don't look into . or .. which sends us into
+		# an infinite loop
+		case $item in
+			.	)
+				continue
+				;;
+			..	)
+				continue
+				;;
+		esac
+
+		# This checks if it's a directory and recursively calls our function on it
+		if test -d $item;
+		then
+			cleanup "$item$s$q$i"
+			continue
+		fi
+
+		remove=false
+
+		case $item in
+			*.o 	)
+				remove=true
+				;;
+
+			*~		)
+				remove=true
+				;;
+
+			*#*#	)
+				remove=true
+				;;
+
+			*akefile	)
+				continue
+		esac
+
+		ftype=$(file -b $item)
+
+		case $ftype in
+			*core*	)
+				remove=true
+				;;
+
+			*executable*	)
+				remove=true
+				;;
+		esac
+
+		if test "$remove" = "true";
+		then
+			echo $item
+		fi
+
+	done
+)
+
+cleanup $*
